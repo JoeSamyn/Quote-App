69772273ac4c0900f0a6627ecb6c8ebd4680b840
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-09 13:30:19 -0700

    Initial Commit

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+
+            //switch (Device.RuntimePlatform)
+            //{
+            //    case Device.iOS:
+            //        Padding = new Thickness(0, 50, 0, 0);
+            //        break;
+
+            //    case Device.Android:
+            //        Padding = 0;
+            //        break;
+
+            //}
+        }
+
+    }
+}
