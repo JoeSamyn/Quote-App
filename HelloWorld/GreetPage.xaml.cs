c50d2907b7c9fdb395473ed80a1f0a062a46097f
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-09 21:12:36 -0700

    Messing around with EventHandlers and StackLayouts

--- HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/GreetPage.xaml.cs
@@ -5,27 +5,38 @@ using Xamarin.Forms;
 
 namespace HelloWorld
 {
+
     public partial class GreetPage : ContentPage
     {
+        int index = 0;
+        private string[] quotes =
+        {
+             "Whatever the mind of man can conceive and believe, it can" +
+             " achieve.",
+             "Two roads diverged in a wood, and I—I took the one less " +
+             "traveled by, And that has made all the difference.",
+             "I attribute my success to this: I never gave or took any" +
+             " excuse.",
+             "You miss 100% of the shots you don’t take."
+        };
 
         public GreetPage()
         {
             InitializeComponent();
 
-            slider.Value = 0.5;
+            slider.Value = 16;
+            label.Text = quotes[0];
 
-            //switch (Device.RuntimePlatform)
-            //{
-            //    case Device.iOS:
-            //        Padding = new Thickness(0, 50, 0, 0);
-            //        break;
-
-            //    case Device.Android:
-            //        Padding = 0;
-            //        break;
-
-            //}
         }
 
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            index++;
+            if(index >= quotes.Length)
+            {
+                index = 0;
+            }
+            label.Text = quotes[index];
+        }
     }
 }
--- HelloWorld/StackPage.xaml.cs
+++ HelloWorld/StackPage.xaml.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Xamarin.Forms;
 
 namespace HelloWorld
 {
-    public partial class MainPage : ContentPage
+    public partial class StackPage : ContentPage
     {
-        public MainPage()
+        public StackPage()
         {
             InitializeComponent();
         }
